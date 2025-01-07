18da0be92f68cb3f0b4ac4d982e0dd1256d083c0
wtto00 <wtto00@outlook.com>
2025-01-07 11:03:50 +0800

    4.44 4.36

--- copy.mjs
+++ copy.mjs
@@ -1,76 +1,88 @@
-import fs from "node:fs";
-import path from "node:path";
-import crypto from "node:crypto";
-import JSON5 from "json5";
+import fs from 'node:fs'
+import path from 'node:path'
+import crypto from 'node:crypto'
+import JSON5 from 'json5'
 
-const filesPath = path.resolve(import.meta.dirname, "files.json");
-const filesContent = fs.readFileSync(filesPath, "utf8");
-const files = JSON.parse(filesContent);
+const filesPath = path.resolve(import.meta.dirname, 'files.json')
+const filesContent = fs.readFileSync(filesPath, 'utf8')
+const files = JSON.parse(filesContent)
 
 /**
  * @param {string} file
  */
 async function md5(file) {
-  const stream = fs.createReadStream(path.resolve(import.meta.dirname, file));
-  const hash = crypto.createHash("md5");
+  const stream = fs.createReadStream(path.resolve(import.meta.dirname, file))
+  const hash = crypto.createHash('md5')
   return new Promise((resolve, reject) => {
-    stream.on("data", (chunk) => {
-      hash.update(chunk, "utf8");
-    });
-    stream.on("end", () => {
-      const md5 = hash.digest("hex");
-      resolve(md5);
-    });
-    stream.on("error", () => {
-      reject(Error(`Failed to get md5 of file ${file}`));
-    });
-  });
+    stream.on('data', (chunk) => {
+      // @ts-ignore
+      hash.update(chunk, 'utf8')
+    })
+    stream.on('end', () => {
+      const md5 = hash.digest('hex')
+      resolve(md5)
+    })
+    stream.on('error', () => {
+      reject(Error(`Failed to get md5 of file ${file}`))
+    })
+  })
 }
 
-const [source, version] = process.argv.slice(2);
+const [source, version] = process.argv.slice(2)
 
 if (!source || !version) {
-  console.log("Unkonwn source or version.");
-  console.log("eg:");
-  console.log("\tnode copy.mjs ~/projects/uniapp-demo 3.0.0-4020920240930001");
-  console.log("\tnode copy.mjs ../projects/uniapp-demo 3.0.0-4020920240930001");
-  process.exit(0);
+  console.log('Unkonwn source or version.')
+  console.log('eg:')
+  console.log('\tnode copy.mjs ~/projects/uniapp-demo 3.0.0-4020920240930001')
+  console.log('\tnode copy.mjs ../projects/uniapp-demo 3.0.0-4020920240930001')
+  process.exit(0)
 }
-const harmonyProject = path.resolve(import.meta.dirname, source, "unpackage/release/com.huayimt.hm");
+const releaseProject = path.resolve(import.meta.dirname, source, 'unpackage/release')
 
-const packagePath = path.join(harmonyProject, "oh-package.json5");
+const harmonyProjectNames = fs.readdirSync(releaseProject)
 
-const packageContent = fs.readFileSync(packagePath, "utf8");
+const harmonyProjectName = harmonyProjectNames.find((name) => fs.existsSync(path.join(releaseProject, name, 'oh-package.json5')))
 
-const packageJson = JSON5.parse(packageContent);
+if (!harmonyProjectName) {
+  console.log('未找到有效的的打包项目，请先在HBuilderX中运行本地打包App-Harmony')
+  process.exit(-1)
+}
 
-const dependencies = packageJson.dependencies;
+const harmonyProject = path.join(releaseProject, harmonyProjectName)
 
-const targetPath = path.resolve(import.meta.dirname, `public/libs/${version}`);
+const packagePath = path.join(harmonyProject, 'oh-package.json5')
+
+const packageContent = fs.readFileSync(packagePath, 'utf8')
+
+const packageJson = JSON5.parse(packageContent)
+
+const dependencies = packageJson.dependencies
+
+const targetPath = path.resolve(import.meta.dirname, `public/libs/${version}`)
 
 if (!fs.existsSync(targetPath)) {
-  fs.mkdirSync(targetPath, { recursive: true });
+  fs.mkdirSync(targetPath, { recursive: true })
 }
 
-const targetList = {};
+const targetList = {}
 
 for (const name in dependencies) {
-  const filePath = path.join(harmonyProject, dependencies[name]);
-  const relativePath = path.relative(harmonyProject, filePath).split(path.sep);
-  if (relativePath[0] !== "libs") continue;
-  const md5string = await md5(filePath);
+  const filePath = path.join(harmonyProject, dependencies[name])
+  const relativePath = path.relative(harmonyProject, filePath).split(path.sep)
+  if (relativePath[0] !== 'libs') continue
+  const md5string = await md5(filePath)
   if (files[md5string]) {
-    targetList[name] = files[md5string];
+    targetList[name] = files[md5string]
   } else {
-    const filename = path.basename(filePath);
-    const targetFilePath = path.join(targetPath, filename);
-    console.log(filePath, targetFilePath);
-    fs.cpSync(filePath, targetFilePath);
-    files[md5string] = `${version}/${filename}`;
-    targetList[name] = files[md5string];
+    const filename = path.basename(filePath)
+    const targetFilePath = path.join(targetPath, filename)
+    console.log(filePath, targetFilePath)
+    fs.cpSync(filePath, targetFilePath)
+    files[md5string] = `${version}/${filename}`
+    targetList[name] = files[md5string]
   }
 }
 
-fs.writeFileSync(path.join(targetPath, "index.json"), JSON.stringify(targetList), "utf8");
+fs.writeFileSync(path.join(targetPath, 'index.json'), JSON.stringify(targetList), 'utf8')
 
-fs.writeFileSync(path.resolve(import.meta.dirname, "files.json"), JSON.stringify(files, null, 2), "utf8");
+fs.writeFileSync(path.resolve(import.meta.dirname, 'files.json'), JSON.stringify(files, null, 2), 'utf8')
